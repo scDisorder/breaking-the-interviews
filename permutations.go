d5bee37a323d50448ff7f9ddb710d3ba99c8e0f4
Dmitrii Limonov <disordervlz@gmail.com>
2023-01-20 11:27:24 +0700

    Update permutation functions

--- permutations.go
+++ permutations.go
@@ -1,39 +1,49 @@
 package main
 
-import "fmt"
+// perInt returns int array permutations
+func permInt(arr []int, left int) [][]int {
+	var helper func([]int, int)
+	var res [][]int
 
-// Perm prints out int array permutations
-func Perm(v []int, f func([]int)) {
-	perm(v, f, 0)
+	helper = func(arr []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+
+	helper(arr, len(arr))
+	return res
 }
 
-func perm(v []int, f func([]int), i int) {
-	if i > len(v) {
-		f(v)
-		return
-	}
-	perm(v, f, i+1)
-	for j := i + 1; j < len(v); j++ {
-		v[i], v[j] = v[j], v[i]
-		perm(v, f, i+1)
-		v[i], v[j] = v[j], v[i]
-	}
-}
+func permString(src string, left int) []string {
+	var res []string
+	str := []rune(src)
 
-func permString(str []rune, left int, right int) {
+	right := len(str) - 1
 	if left == right {
-		fmt.Println(string(str)) // outputs permutation
+		res = append(res, string(str))
 	} else {
 		for i := left; i <= right; i++ {
 			str[left], str[i] = str[i], str[left]
-			permString(str, left+1, right)
+			res = append(res, permString(string(str), left+1)...)
 			str[left], str[i] = str[i], str[left]
 		}
 	}
-}
 
-// returns all the permutations of "qwer"
-func stringPermutations() {
-	str := "qwer"
-	permString([]rune(str), 0, len(str)-1)
+	return res
 }
---
+++ permutations_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_permString(t *testing.T) {
+	type args struct {
+		src  string
+		left int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "test-01",
+			args: args{
+				src:  "abc",
+				left: 0,
+			},
+			want: []string{"abc", "acb", "bac", "bca", "cba", "cab"},
+		},
+		{
+			name: "test-02",
+			args: args{
+				src:  "qwe",
+				left: 0,
+			},
+			want: []string{"qwe", "qew", "wqe", "weq", "ewq", "eqw"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fmt.Println(permString(tt.args.src, tt.args.left))
+			if got := permString(tt.args.src, tt.args.left); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("permString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_permInt(t *testing.T) {
+	type args struct {
+		v []int
+		i int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test-03",
+			args: args{
+				v: []int{1, 2, 3},
+				i: 0,
+			},
+			want: [][]int{{3, 1, 2}, {1, 3, 2}, {2, 1, 3}, {1, 2, 3}, {2, 3, 1}, {3, 2, 1}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fmt.Println(permInt(tt.args.v, tt.args.i))
+			if got := permInt(tt.args.v, tt.args.i); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("permInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
