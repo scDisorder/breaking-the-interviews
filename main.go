523bc1cf874c10701af2b43fc87b438b4da32e8a
Dmitrii Limonov <disordervlz@gmail.com>
2023-02-07 14:18:03 +0700

    Update main.go

--- main.go
+++ main.go
@@ -7,9 +7,5 @@ import (
 func main() {
 	fmt.Println("echo fmt")
 
-	//nums := []int{1, 2, 1, 2}
-	//nums2 := []int{5, -3, 5}
-	//fmt.Println(leetcode.MaxSubarraySumCircular(nums))
-	//fmt.Println(leetcode.MaxSubarraySumCircular(nums2))
-
+	// place example calls here
 }
